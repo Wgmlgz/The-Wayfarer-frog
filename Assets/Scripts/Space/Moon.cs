93daf55315d70c463e9f3f3ab78738b819a95bc4
Wgmlgz <62064707+Wgmlgz@users.noreply.github.com>
2020-08-15 03:24:30 +0300

    b

--- Assets/Scripts/Space/Moon.cs
+++ Assets/Scripts/Space/Moon.cs
@@ -6,7 +6,10 @@ public class Moon : MonoBehaviour
 {
     [Range(0, 7)] public int phase = 1;
     public Sprite[] sequence;
-
+    private void Start()
+    {
+        phase = Random.Range(0, 8);
+    }
     private void Update()
     {
         GetComponent<SpriteRenderer>().sprite = sequence[phase];
--- Assets/Scripts/ZhabaSelector.cs
+++ Assets/Scripts/ZhabaSelector.cs
@@ -8,24 +8,33 @@ public class Arrows
     public GameObject ar1;
     public GameObject ar2;
 }
-
+[System.Serializable]
+public class Zhabka
+{
+    public GameObject skin;
+    public List<GameObject> hats;
+}
 public class ZhabaSelector : MonoBehaviour
 {
     public int viewN;
 
     public int selectedN;
+    public int selectedHat;
 
     public List<bool> isLock;
     public List<Cinemachine.CinemachineVirtualCamera> cams;
     public List<Arrows> ars;
     public List<TMPro.TextMeshProUGUI> staT;
     public List<GameObject> locks;
-    public List<GameObject> toadSkins;
+    public List<Zhabka> toads;
+    public List<GameObject> hatDA;
+    public GameObject hatNET;
 
     private void Start()
     {
-        selectedN = PlayerPrefs.GetInt("SelectedToad");
-        SelectToad(selectedN);
+        SetHat(PlayerPrefs.GetInt("SelectedHat"));
+        SelectToad(PlayerPrefs.GetInt("SelectedToad"));
+
 
         for (int i = 0; i < locks.Count; i++)
         {
@@ -83,11 +92,13 @@ public class ZhabaSelector : MonoBehaviour
         }
         staT[j].SetText("selected");
 
-        foreach (var i in toadSkins)
+        foreach (var i in toads)
         {
-            i.SetActive(false);
+            i.skin.SetActive(false);
         }
-        toadSkins[j].SetActive(true);
+        toads[j].skin.SetActive(true);
+
+        SetHat(selectedHat);
     }
     public void LockToad(int j)
     {
@@ -98,5 +109,23 @@ public class ZhabaSelector : MonoBehaviour
     {
         locks[j].SetActive(false);
         PlayerPrefs.SetInt("ToadLock" + j.ToString(), 0);
+    }
+    public void SetHat(int j)
+    {
+        selectedHat = j;
+        PlayerPrefs.SetInt("SelectedHat", j);
+
+        foreach (var i in toads[selectedN].hats)
+        {
+            i.SetActive(false);
+        }
+        if(j != -1) toads[selectedN].hats[j].SetActive(true);
+
+        foreach (var i in hatDA)
+        {
+            i.SetActive(false);
+        }
+        if(j != -1) hatDA[j].SetActive(true);
+        hatNET.SetActive(j == -1);
     }
 }
